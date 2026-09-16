57c1a09d8dcf717385ca099679e9f9291597c867
Huseyin Karamelikli <hakperest@gmail.com>
2026-09-16 15:30:45 +0300

    Deprecate imf_example_data for backward compatibility

---
+++ R/old_versions_data.R
@@ -0,0 +1,20 @@
+#' IMF Example Data (Deprecated)
+#'
+#' @description
+#' \lifecycle{deprecated}
+#'
+#' This dataset is \strong{deprecated} and is provided for testing purposes only
+#' for versions of \pkg{kardl} before 2.0.5. It is retained for backward
+#' compatibility.
+#'
+#' @format A data frame with 470 rows and 4 variables:
+#' \describe{
+#'   \item{ER}{Numeric. Exchange rate of Turkey.}
+#'   \item{CPI}{Numeric. CPI of Turkey.}
+#'   \item{PPI}{Numeric. PPI of Turkey.}
+#'   \item{covid}{Integer. COVID-19 dummy variable.}
+#' }
+#' @examples
+#' data(imf_example_data)
+#' head(imf_example_data)
+"imf_example_data"
